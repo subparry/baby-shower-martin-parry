4d91ee7709545694de541e672e6949ec877a833e
Adrian Parry Ainardi <subparry@gmail.com>
2019-03-01 00:16:51 -0300

    otro

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,16 @@
 class GuestsController < ApplicationController
   def index
+    @guest = Guest.new
     @gifts = Gift.all
+  end
+
+  def create
+    @guest = Guest.new(guest_params)
+    if @guest.save
+      session[:user_id] = @guest.id
+      flash[:notice] = "Ha ingresado como #{@guest.name}!"
+      redirect_to root_path
+    end
   end
 
   def take
@@ -8,5 +18,17 @@ class GuestsController < ApplicationController
     @gift.update(taken: true)
     flash[:notice] = "Has decidido regalar #{@gift.name}!"
     redirect_to root_path
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = "Ha salido de su sesión!"
+    redirect_to root_path
+  end
+
+  private
+
+  def guest_params
+    params.require(:guest).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'guests#index'
   post 'guests/take_gift', to: 'guests#take', as: 'take_gift'
+  post 'guests/create', to: 'guests#create', as: 'guests'
+  get 'guests/destroy', to: 'guests#destroy', as: 'destroy_session'
 end
